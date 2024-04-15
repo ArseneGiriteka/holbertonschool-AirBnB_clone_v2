3b3fa5c0e7a10b2d7ab23f40110357027b8b2b26
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-04-15 11:52:02 +0200

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module defines a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+def states_list():
+    """Displays an HTML page with a list of all States objects"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>')
+def states_id(id):
+    """Displays an HTML page with a list of all States and Cities objects"""
+    states = storage.all(State).values()
+    for state in states:
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html')
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
